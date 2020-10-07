2aa7b867615b3acecd5b8d85c2b58a80f18f8fe9
bettilaszlo <laszlobetti3@gmail.com>
2020-10-07 13:59:09 +0200

    excluded.csv beolvasása és eltárolása

--- dhcp/Program.cs
+++ dhcp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,33 @@ namespace dhcp
 {
     class Program
     {
+        static List<IP> excluded = new List<IP>();
+        static void BeolvasExcuded()
+        {
+            try
+            {
+                StreamReader file = new StreamReader("excluded.csv");
+
+                while (!file.EndOfStream)
+                {
+                    excluded.Add(new IP(file.ReadLine()));
+                }
+               
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
         static void Main(string[] args)
         {
-            var ip = new IP("192.168.10.100");
+            BeolvasExcuded();
 
-            Console.WriteLine(ip.IPSzoveg);
-            Console.WriteLine(ip.CimPluszEgy());
+            foreach (var ex in excluded)
+            {
+                Console.WriteLine(ex.OK4);
+            }
 
 
             Console.ReadKey();
