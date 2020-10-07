12a79dbfa5d5ccb44a66c37f796cd55f74859c3f
bettilaszlo <laszlobetti3@gmail.com>
2020-10-07 14:38:54 +0200

    reserved.csv állomány beolvasása és eltárolása

--- dhcp/Program.cs
+++ dhcp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace dhcp
     class Program
     {
         static List<IP> excluded = new List<IP>();
+        static Dictionary<string, IP> reserved = new Dictionary<string, IP>();
         static void BeolvasExcuded()
         {
             try
@@ -28,15 +30,34 @@ namespace dhcp
                 Console.WriteLine(ex.Message);
             }
         }
+        static void BeolvasReserved()
+        {
+            try
+            {
+                StreamReader file = new StreamReader("reserved.csv");
+                
+                while (!file.EndOfStream)
+                {
+                    string[] adat = file.ReadLine().Split(';');
+                    reserved.Add(adat[0], new IP(adat[1]));
+                }
+
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
         static void Main(string[] args)
         {
             BeolvasExcuded();
-
-            foreach (var ex in excluded)
+            BeolvasReserved();
+            foreach (var r in reserved)
             {
-                Console.WriteLine(ex.OK4);
+                Console.WriteLine(r.Key+"--->"+r.Value.IPSzoveg);
             }
-
 
             Console.ReadKey();
         }
